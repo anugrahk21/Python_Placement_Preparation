65bd0bb17602600f400bed53f477736da42a37c9
ANUGRAH K <anugrah.k910@gmail.com>
2026-01-21 09:47:21 +0530

    Enhance comments for basic data types and introduce other data structures in Python

--- P_1.py
+++ P_1.py
@@ -2,6 +2,9 @@ print("Hello World")
 
 #Different types of data structures in Python
 
+#Basic data types : Integer, String, Float, Boolean
+#These are the fundamental data types in Python. They can be used to store single values.
+
 # Integer
 age=int(input("Enter something: "))
 print("You entered:", age)
@@ -17,3 +20,6 @@ print("Your height is:", height)
 # Boolean
 is_student=input("Are you a student? (yes/no): ").strip()=="yes"
 print("Is student:", is_student)
+
+#Other data structures: List, Tuple, Set, Dictionary
+#These are more complex data structures that can hold multiple values.
