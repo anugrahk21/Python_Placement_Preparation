43db8717ad876ec01f531c9816f6314806d0f764
ANUGRAH K <anugrah.k910@gmail.com>
2026-01-21 10:50:14 +0530

    Add functionality to find the largest of three numbers.

--- P_2.py
+++ P_2.py
@@ -30,3 +30,15 @@ for i in item_details:
     print(i)
     #or
     print(f"Price per item of {i[0]} is {i[1]} and final cost including GST is {i[2]}")
+
+
+#Largest of three numbers
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+num3=int(input("Enter third number: "))
+if (num1>num2) and (num1>num3):
+    print(f"{num1} is the largest number")
+elif (num2>num1) and (num2>num3):
+    print(f"{num2} is the largest number")
+else:
+    print(f"{num3} is the largest number")
