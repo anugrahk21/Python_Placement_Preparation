fdd223fea256b2767369a9a5006a787a363e2af5
ANUGRAH K <anugrah.k910@gmail.com>
2026-01-21 09:44:33 +0530

    Add example of different data structures in Python

--- P_1.py
+++ P_1.py
@@ -1,1 +1,19 @@
 print("Hello World")
+
+#Different types of data structures in Python
+
+# Integer
+age=int(input("Enter something: "))
+print("You entered:", age)
+
+# String
+name=input("Enter your name: ")
+print("Hello,", name)
+
+# Float
+height=float(input("Enter your height including decimal: "))
+print("Your height is:", height)
+
+# Boolean
+is_student=input("Are you a student? (yes/no): ").strip()=="yes"
+print("Is student:", is_student)
