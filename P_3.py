9ac5107c68c9415bf2f14cd562caee022dcb60b7
ANUGRAH K <anugrah.k910@gmail.com>
2026-01-22 10:39:17 +0530

    Refactor password validation program and add ATM cash dispensing and greeting functionality

--- P_3.py
+++ P_3.py
@@ -1,9 +1,39 @@
 # Password validation program
+
+correct_pass="admin123"
 while True:
     entered_pass=input("Enter your password: ")
-    correct_pass="admin123"
     if (entered_pass==correct_pass):
         print("Access Granted")
         break
     else:
         print("Access Denied")
+
+# ATM dispensing cash
+
+withdraw_amount=int(input("Enter amount to withdraw: "))
+if (withdraw_amount<=0):
+    print("Enter a valid amount")
+elif (withdraw_amount % 200 != 0 and withdraw_amount % 500 != 0 and (withdraw_amount < 200 and withdraw_amount < 500)):
+    print("Amount should be in multiples of 200 or 500")
+   
+# if withdraw_amount % 200 == 0 or withdraw_amount % 500 == 0 or withdraw_amount >= 700:
+#     print("Discharging Amount:", withdraw_amount)
+
+else:
+    print("Discharging Amount:", withdraw_amount)
+
+
+# Wishing program
+
+time=int(input("Enter time in 24-hour format (0-23): "))
+if (time<=0 or time>23):
+    print("Enter valid time")
+elif(time>=5 and time<12):
+    print("Good Morning")
+elif(time>=12 and time<16):
+    print("Good Afternoon")
+elif(time>=16 and time<19):
+    print("Good Evening")
+else:
+    print("Good Night")
