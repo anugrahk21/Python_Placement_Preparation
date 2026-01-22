6f4d32fb2c8ce92ff7cd94ee3731a2f084df5c29
ANUGRAH K <anugrah.k910@gmail.com>
2026-01-22 14:30:18 +0530

    Add string and its manipulation examples in P_4.py

---
+++ P_4.py
@@ -0,0 +1,16 @@
+#String
+
+S="Hello, World!"
+print(S)
+
+# Indexing
+print(S[7])  # W
+
+# Slicing
+print(S[0:5])  # Hello
+
+# Reversing a string
+print(S[::-1]) # !dlroW ,olleH
+
+# Length of string
+print(len(S))   #13
