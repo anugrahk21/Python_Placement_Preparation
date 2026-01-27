d35704c5320800c6907cdee15d4c4180e2029d0b
ANUGRAH K <anugrah.k910@gmail.com>
2026-01-27 14:20:36 +0530

    Refactor Bank class; Improve code readability and consistency in method calls

--- P_8.py
+++ P_8.py
@@ -1,27 +1,32 @@
 # OOPs in Python
 
 class Bank:
+    # Constructor
     def __init__(self,name,amount=0):
         self.name=name
-        self.amount=amount
+        self.amount=amount  # Balance amount, initially 0
+
+    # self represents the current object
+
+    # Member Functions (Methods)
     def deposit(self,dep_amount):
         self.amount+=dep_amount
         print(f"Amount :{dep_amount} deposited successfully")
+        self.balance()
     def withdraw(self,wd_amount):
         if(wd_amount>self.amount):
             print("Insufficient balance")
             return
         self.amount-=wd_amount
         print(f"Amount :{wd_amount} withdrawn successfully")
+        self.balance()
     def balance(self):
-        print(f"Available balance is :{self.amount}")
+        print(f"Available balance is :{self.amount}\n")
     
 if __name__ == "__main__":
     name=input("Enter your name:")
-    user1=Bank(name)
+    user=Bank(name)
     dep_amount=int(input("Enter amount to be deposited:"))
-    user1.deposit(dep_amount)
-    user1.balance()
+    user.deposit(dep_amount)
     wd_amount=int(input("Enter amount to be withdrawn:"))
-    user1.withdraw(wd_amount)
-    user1.balance()
+    user.withdraw(wd_amount)
